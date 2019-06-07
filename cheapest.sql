374845da039409417cf0928d5a35705175b91969
Lonbasian <49562643+Afewerk@users.noreply.github.com>
2019-06-07 10:56:26 -0500

    sql_cheapest_times

---
+++ cheapest.sql
@@ -0,0 +1,21 @@
+--indexing 
+--create index first_index5 on cal_price (price);
+--creating an intermediate table
+with cheapest as 
+(
+select   
+	price, 
+	date(calender_date) as cheap_date
+	from sfo_calendar
+	group by 1,2
+)
+select 
+	sa.listing_id,
+	sa.price,
+	date(sa.calender_date)
+from cal_price sa
+	
+inner join cheapest ch
+on date(sa.calender_date) = ch.cheap_date
+where sa.price between 10 and 236
+order by price limit 10000;
